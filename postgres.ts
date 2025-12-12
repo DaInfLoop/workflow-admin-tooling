485628fdc9e22e6b7a3012edb373abf1e3a240c9
DaInfLoop <github@dainfloop.is-a.dev>
2025-12-12 22:25:09 +0000

    Add postgres

---
+++ postgres.ts
@@ -0,0 +1,10 @@
+import postgres from "postgres";
+
+const sql = postgres({
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD
+});
+
+export default sql;
